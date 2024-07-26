f831df3c82285c001448a43d49ed86db308a2f79
bursiaryn <127074874+bursiaryn@users.noreply.github.com>
2024-07-26 10:22:56 +0700

    sambungan.py

--- sambungan.py
+++ sambungan.py
@@ -1,24 +1,28 @@
 import pickle
 import streamlit as st
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 
 # Membaca model
-customer_model = pickle.load(open('pelanggan_model.sav','rb'))
+file_path = 'pelanggan_model.sav'
+
+# Membaca file dan memuat data
+with open(file_path, 'rb') as file:
+    data = pickle.load(file)
+
 
 # Judul web
 st.title('Prediksi Status Pelanggan')
 
 # Input data dengan contoh angka valid untuk pengujian
-CreditScore = st.text_input('Uang kredit pelanggan')
-Gender = st.text_input('Suhu Maksimum')
-Age = st.text_input('Suhu Minimum')
-Tenure = st.text_input('Kecepatan Angin')
-Balance = st.text_input('Uang kredit pelanggan')
-NumOfProducts = st.text_input('Suhu Maksimum')
-HasCrCard = st.text_input('Suhu Minimum')
-IsActiveMember = st.text_input('Kecepatan Angin')
-EstimatedSalary = st.text_input('Kecepatan Angin')
+CreditScore = st.text_input('Uang kredit pelanggan', key= 'CreditScore')
+Gender = st.selectbox('Jenis kelamin', options=[(0, 'Perempuan'), (1, 'Laki-laki')], format_func=lambda x: x[1],key = 'Gender')
+Age = st.text_input('Umur',key = 'Age')
+Tenure = st.text_input('Masa Jabatan',key = 'Tenure')
+Balance = st.text_input('Uang kredit pelanggan',key = 'Balance')
+NumOfProducts = st.selectbox('Jumlah Produk Bank', options=[1, 2, 3, 4],key = 'NumOfProducts')
+HasCrCard = st.selectbox('Kepunyaan Kartu Kredit', options=[(0, 'Tidak'), (1, 'Ya')],format_func=lambda x: x[1],key = 'HasCrCard')
+IsActiveMember = st.selectbox('Keaktifan Member', options=[(0, 'Tidak Aktif'), (1, 'Aktif')], format_func=lambda x: x[1], key = 'IsActiveMember')
+EstimatedSalary = st.text_input('Perkiraan Gaji',key='EstimatedSalary')
 
 Prediksi_Status_Pelanggan = ''
 
@@ -26,11 +30,10 @@ Prediksi_Status_Pelanggan = ''
 if st.button('Prediksi'):
     try:
         # Konversi input menjadi numerik
-        inputs = np.array([[float(CreditScore), float(Gender), float(Age), 
-                            float(Tenure),float(Balance), float(NumOfProducts), float(HasCrCard), 
-                            float(IsActiveMember), float(EstimatedSalary)]])
+        inputs = np.array([[float(CreditScore), Gender[0], float(Age), float(Tenure),float(Balance),float(NumOfProducts), 
+                            HasCrCard[0], IsActiveMember[0], float(EstimatedSalary)]])
         # Lakukan prediksi
-        status_prediksi = customer_model.predict(inputs)
+        status_prediksi = data.predict(inputs)
         
         if status_prediksi[0] == 0:
             status_prediksi = 'Pelanggan Tidak Keluar dari Bank'
